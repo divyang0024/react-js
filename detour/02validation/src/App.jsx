8093d465a133f3463154ec08bb9914261b0d9c36
Divyang <divyang0024@gmail.com>
2024-05-13 22:17:40 +0530

    more yup validation

--- detour/02validation/src/App.jsx
+++ detour/02validation/src/App.jsx
@@ -5,15 +5,22 @@ function App() {
   const initData = {
     name: "",
     email: "",
+    age: "",
+    password: "",
+    cpassword: "",
   };
-  const { handleSubmit, handleChange, values, errors } = useFormik({
-    initialValues: initData,
-    onSubmit: (values) => {
-      console.log(values.name);
-      console.log(values.email);
-    },
-    validationSchema: formSchema,
-  });
+  const { handleSubmit, handleChange, handleBlur, values, errors, touched } =
+    useFormik({
+      initialValues: initData,
+      onSubmit: (values, action) => {
+        console.log(values.name);
+        console.log(values.email);
+        console.log(values.age);
+        console.log(values.password);
+        action.resetForm();
+      },
+      validationSchema: formSchema,
+    });
   return (
     <>
       <form onSubmit={handleSubmit}>
@@ -23,9 +30,12 @@ function App() {
           name="name"
           id="nameField"
           onChange={handleChange}
+          onBlur={handleBlur}
           value={values.name}
         />
-        <span style={{ color: "red" }}>{errors.name}</span>
+        {touched.name && errors.name ? (
+          <span style={{ color: "red" }}>{errors.name}</span>
+        ) : null}
         <br />
         <label htmlFor="emailField">Email : </label>
         <input
@@ -33,9 +43,52 @@ function App() {
           name="email"
           id="emailField"
           onChange={handleChange}
+          onBlur={handleBlur}
           value={values.email}
         />
-        <span style={{ color: "red" }}>{errors.email}</span>
+        {touched.email && errors.email ? (
+          <span style={{ color: "red" }}>{errors.email}</span>
+        ) : null}
+        <br />
+        <label htmlFor="ageField">Age : </label>
+        <input
+          type="text"
+          name="age"
+          id="ageField"
+          onChange={handleChange}
+          onBlur={handleBlur}
+          value={values.age}
+        />
+        {touched.age && errors.age ? (
+          <span style={{ color: "red" }}>{errors.age}</span>
+        ) : null}
+        <br />
+        <label htmlFor="passwordField">password : </label>
+        <input
+          type="password"
+          name="password"
+          id="passwordField"
+          onChange={handleChange}
+          onBlur={handleBlur}
+          value={values.password}
+        />
+        {touched.password && errors.password ? (
+          <span style={{ color: "red" }}>{errors.password}</span>
+        ) : null}
+        <br />
+        <label htmlFor="cpasswordField">Confirm Password : </label>
+        <input
+          type="password"
+          name="cpassword"
+          id="cpasswordField"
+          onChange={handleChange}
+          onBlur={handleBlur}
+          value={values.cpassword}
+        />
+        {touched.cpassword && errors.cpassword ? (
+          <span style={{ color: "red" }}>{errors.cpassword}</span>
+        ) : null}
+        <br />
         <button type="submit">Go</button>
       </form>
     </>
--- detour/02validation/src/FormSchema.js
+++ detour/02validation/src/FormSchema.js
@@ -8,4 +8,15 @@ export const formSchema = Yup.object({
   email: Yup.string()
     .email("not a valid email")
     .required("cannot leave email field empty"),
+  age: Yup.number()
+    .min(10, "too young")
+    .max(50, "too old")
+    .required("cannot leave age field empty"),
+  password: Yup.string().matches(
+    /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/,
+    "password too weak"
+  ),
+  cpassword: Yup.string()
+    .oneOf([Yup.ref("password"), null], "re-check password")
+    .required("cannot leave confirm password field empty"),
 });
